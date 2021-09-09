fa067a2b8502f8643eae5227b9f836285866a51c
Tran Thi Nguyet Ha <hattn@vietis.com.vn>
2021-09-09 16:28:48 +0700

    add db script

---
+++ sql/database.sql
@@ -0,0 +1,64 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : spring-boot-demo
+ Source Server Type    : MySQL
+ Source Server Version : 80025
+ Source Host           : localhost:3306
+ Source Schema         : springcrud
+
+ Target Server Type    : MySQL
+ Target Server Version : 80025
+ File Encoding         : 65001
+
+ Date: 08/09/2021 17:38:34
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for department
+-- ----------------------------
+DROP TABLE IF EXISTS `department`;
+CREATE TABLE `department`  (
+  `id` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `founding_year` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of department
+-- ----------------------------
+INSERT INTO `department` VALUES ('CS', 'Computer Science', '2020');
+INSERT INTO `department` VALUES ('MC', 'Multimedia Communication', '2020');
+INSERT INTO `department` VALUES ('SE', 'Software Engineering', '2020');
+
+-- ----------------------------
+-- Table structure for student
+-- ----------------------------
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `gender` bit(1) NULL DEFAULT NULL,
+  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `dept_id` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `FK_dept_id`(`dept_id`) USING BTREE,
+  CONSTRAINT `FK_dept_id` FOREIGN KEY (`dept_id`) REFERENCES `department` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of student
+-- ----------------------------
+INSERT INTO `student` VALUES (1, 'a', b'1', '0123', 'SE');
+INSERT INTO `student` VALUES (2, 'b', b'1', '0123', 'SE');
+INSERT INTO `student` VALUES (3, 'c', b'0', '1234', 'MC');
+INSERT INTO `student` VALUES (4, 'd', b'0', '1234', 'CS');
+INSERT INTO `student` VALUES (5, 'new student', b'1', '01234567', 'SE');
+INSERT INTO `student` VALUES (7, 'updated student', b'1', '01234567', 'MC');
+INSERT INTO `student` VALUES (8, 'update student 8', b'1', '01234567', 'CS');
+
+SET FOREIGN_KEY_CHECKS = 1;
